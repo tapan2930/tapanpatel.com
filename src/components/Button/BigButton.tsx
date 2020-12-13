6ffc7c6e01927d2d86ea00c697202e6b512fe281
Tapan Patel <tapan2930@gmail.com>
2020-12-14 00:14:51 +0530

    home page fixed 2

--- src/components/Button/BigButton.tsx
+++ src/components/Button/BigButton.tsx
@@ -10,7 +10,7 @@ interface IProps {
 
     return (
       <Link href={`${link}`}>
-        <a  className={`transition duration-200 transform hover:-translate-y-2 hover:shadow-md ease-in-out big-button text-center p-8 lg:p-18 text-white w-full h-full text-2xl lg:text-6xl flex flex-col items-center justify-center cursor-pointer`}>
+        <a  className={`transition duration-200 transform hover:-translate-y-2 shadow-md hover:shadow-sm ease-in-out big-button text-center p-8 lg:p-16 text-white w-full h-full text-2xl lg:text-6xl flex flex-col items-center justify-center cursor-pointer`}>
         <span className="align-middle">{text}</span>
         </a>
         </Link>
