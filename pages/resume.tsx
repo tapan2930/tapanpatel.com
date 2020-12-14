095fa348e75d1beeece06b007446c967e3db329a
Tapan Patel <tapan2930@gmail.com>
2020-12-14 12:55:56 +0530

    fixed resume layout 2

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -11,8 +11,10 @@ function ResumeDisplay() {
     return (
         <div className="bg-primary text-text font-body pb-16">
             <Navbar />
-            <div className="container mx-auto px-2 sm:px-10 md:px-10 lg:px-72 flex justify-center mb-3">
-            <Button link={"assets/Resume.pdf"} text={"Download"} />
+            <div className="container mx-auto px-2 sm:px-10 md:px-10 lg:px-72 flex justify-center mb-3 flex-col">
+           <div className="inline-block">
+           <Button link={"assets/Resume.pdf"} text={"Download"} />
+             </div>
             <>
       <div className="mt-6 overflow-scroll md:overflow-hidden ">      
         <Document
