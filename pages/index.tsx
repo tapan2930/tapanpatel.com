cc87858970180756247a352c284435f0d862c0eb
Tapan Patel <tapan2930@gmail.com>
2021-01-13 22:26:55 +0530

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,6 @@ const IndexPage: NextPage = () => {
   return (
     <main className="bg-primary text-white font-body">
       <Head>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NNGD2XDZFF"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-NNGD2XDZFF');
-</script>
       <meta property="og:url" content="https://pateltapan.com/" />
       <meta property="og:type" content="web" />
       <meta property="og:title" content="Tapan Patel Portfolio" />
