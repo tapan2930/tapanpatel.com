f1380895645f88d65728d56dbde6d6cb07651f14
Tapan Patel <tapan2930@gmail.com>
2021-02-18 14:59:23 +0530

    added metetags

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,26 @@ const IndexPage: NextPage = () => {
   return (
     <main className="bg-primary text-white font-body">
       <Head>
-      <meta property="og:url" content="https://pateltapan.com/" />
-      <meta property="og:type" content="web" />
-      <meta property="og:title" content="Tapan Patel Portfolio" />
-      <meta property="og:image" content="/og.png" />
+        <meta name="title" content="Tapan Patel | Fullstack Developer, Wants to create an amazing User Experience"/>
+        <meta name="description" content="I am a Fullstack developers, Building great user interface, scaleable and amazing apps."/>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://pateltapan.com/"/>
+        <meta property="og:title" content="Tapan Patel | Fullstack Developer"/>
+        <meta property="og:description" content="I am a Fullstack developers, Building great user interface, scaleable and amazing apps."/>
+        <meta property="og:image" content="https://pateltapan.com/og.png"/>
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="https://pateltapan.com/og.png"/>
+        <meta property="twitter:url" content="https://pateltapan.com/" />
+        <meta property="twitter:title" content="Tapan Patel | Fullstack Developer"/>
+        <meta property="twitter:description" content="I am a Fullstack developers, Building great user interface, scaleable and amazing apps."/>
+        <meta property="twitter:image" content="https://pateltapan.com/og.png"/>
+
       <link rel="icon" href="/fevicon.ico" />
       <title>Tapan Patel</title>
+
       </Head>
       <Header />
     </main>
