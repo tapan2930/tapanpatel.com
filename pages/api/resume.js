1c0ddd73e9bc711217017ea3f211bed7ec0472b6
Tapan Patel <tapan2930@gmail.com>
2021-08-10 10:41:01 +0530

    fixed resume

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,5 +1,5 @@
 import CloudConvert from 'cloudconvert';
-const cloudConvert = new CloudConvert(process.env.CLOUDCONVERTKEY, true);
+const cloudConvert = new CloudConvert(process.env.CLOUDCONVERTKEY);
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         // Process a POST request
